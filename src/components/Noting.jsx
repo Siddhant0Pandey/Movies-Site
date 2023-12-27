ec63370fe7feb7087633aa11b8e6de31fadbed09
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2023-12-27 17:16:08 +0545

    nothing setup

--- src/components/Noting.jsx
+++ src/components/Noting.jsx
@@ -3,6 +3,10 @@ export default function Noting() {
     <div>
       This is nothing component
       <h2>This is going to show nothing on the browser.</h2>
+      <p>
+        Helo everyone welcome to mhy channel today I am going to show you some
+        of the magic triks
+      </p>
     </div>
   );
 }
