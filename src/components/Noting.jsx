89bb96d6e56b31c745d9a640e82626c0114db0f0
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2023-12-27 17:15:01 +0545

    nothing

--- src/components/Noting.jsx
+++ src/components/Noting.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
 export default function Noting() {
-  return <div>This is nothing component</div>;
+  return (
+    <div>
+      This is nothing component
+      <h2>This is going to show nothing on the browser.</h2>
+    </div>
+  );
 }
