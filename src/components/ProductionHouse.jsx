6bf80bc335f85e1fa52624713395e424cda238f7
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2023-12-28 11:58:55 +0545

    production setup

---
+++ src/components/ProductionHouse.jsx
@@ -0,0 +1,70 @@
+import disney from "../assets/Images/disney.png";
+import pixar from "../assets/Images/pixar.png";
+import marvel from "../assets/Images/marvel.png";
+import starwar from "../assets/Images/starwar.png";
+import nationalG from "../assets/Images/nationalG.png";
+
+import starwarV from "../assets/Videos/star-wars.mp4";
+import disneyV from "../assets/Videos/disney.mp4";
+import marvelV from "../assets/Videos/marvel.mp4";
+import nationalGeographicV from "../assets/Videos/national-geographic.mp4";
+import pixarV from "../assets/Videos/pixar.mp4";
+
+function ProductionHouse() {
+  const productionHouseList = [
+    {
+      id: 1,
+      image: disney,
+      video: disneyV,
+    },
+    {
+      id: 2,
+      image: pixar,
+      video: pixarV,
+    },
+    {
+      id: 3,
+      image: marvel,
+      video: marvelV,
+    },
+    {
+      id: 4,
+      image: starwar,
+      video: starwarV,
+    },
+    {
+      id: 5,
+      image: nationalG,
+      video: nationalGeographicV,
+    },
+  ];
+  return (
+    <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
+      {productionHouseList.map((item) => (
+        <div
+          key={item.id}
+          className="border-[2px] border-secondaryColor
+        rounded-lg hover:scale-110 transition-all duration-300
+        ease-in-out cursor-pointer relative shadow-md 
+        shadow-green-800"
+        >
+          <img
+            src={item.image}
+            className="w-full"
+            alt={`Production House ${item.id}`}
+          />
+          <video
+            src={item.video}
+            autoPlay
+            loop
+            playsInline
+            muted
+            className="absolute top-0 opacity-0 hover:opacity-35"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductionHouse;
